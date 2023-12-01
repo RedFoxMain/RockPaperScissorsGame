41a07a82460ee2a222f687f76868a0924d657e2e
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2023-12-01 22:19:47 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,70 +1,41 @@
 //Сделано RedFoxMain 
 //Git hub: https://github.com/RedFoxMain
 
-
 #include <iostream>
 #include <string>
-#include <random>
-#include <array>
-#include <iterator>
-using namespace std;
 
-random_device rd;
-mt19937 gen(rd());
- 
-int random(int low, int high)
-{
-    uniform_int_distribution<> dist(low, high);
-    return dist(gen);
+
+std::string botStep(){
+                std::string array[3] = {"Камень","Ножницы","Бумага
+"};
+        int index = rand() % 3;
+        return array[index];
 }
 
-string botStep(){
-	array ar = {"Камень","Ножницы","Бумага"};
-	int r = random(0,2);
-	return ar[r];
-}
+void showWinner(std::string player, std::string bot){
+        if (player == bot){
+                        std::cout << "Ничья";
+        }
+        else if ((player == "Ножницы" && bot == "Бумага") ||        (player == "Камень" && bot == "Ножницы") ||
+                                 (player == "Бумага" && bot == "Ка
+мень")){
 
-void showWinner(string player, string bot){
-	if (player == bot){
-		cout << "Ничья";
-	}
-	else if (player == "Ножницы"){
-		if (bot == "Камень"){
-			cout << "Бот победил";
-		}else{
-			cout << "Игрок победил";
-		}
-	}
-	else if (player == "Камень"){
-		if (bot == "Бумага"){
-			cout << "Бот победил";
-		}else{
-			cout << "Игрок победил";
-		}
-	}
-	else if (player == "Бумага"){
-		if (bot == "Ножница"){
-			cout << "Бот победил";
-		}else{
-			cout << "Игрок победил";
-		}
-	
-	}
+                        std::cout << "Игрок победил";
+                }else{
+             std::cout << "Бот победил";
+        }
+                std::cout << std::endl;
 }
 
 int main() {
-	string symb;
-	cout << "Камень, Ножницы или Бумага: " << endl;
-	while (symb != "exit"){
-		cout << endl;
-		cout << "Игрок: ";
-		cin >> symb;
-		if (symb != "exit"){
-		string bot = botStep();
-		cout << "Бот: " << bot << endl;
-		showWinner(symb, bot);
-		}
-	}
-	
+                std::string comand;
+                std::cout << "Камень, Ножницы или Бумага: " << std
+::endl;
+        while (comand != "exit"){
+                        std::cout << "Игрок: ";
+            std::cin >> comand;
+                        std::string bot = botStep();
+                        std::cout << "Бот: " << bot << std::endl;
+            showWinner(comand, bot);
+        }
 }
-
